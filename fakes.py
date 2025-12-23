04429a038ea9d0d27bdf6bb8795dc4b61510e8a9
edwa5158 <jasonedwards2011@gmail.com>
2025-12-23 14:02:49 -0600

    added a ChatbotSettings class to wrap argparse and insert logging of settings

--- fakes.py
+++ fakes.py
@@ -4,7 +4,7 @@ class FakeMetadata():
     candidates_token_count: int = 0
 
 class FakeResponse():
-    def __init__(self, prompt_token_count: int = 0, candidates_token_count: int = 0, response_text: str = ""):
+    def __init__(self, prompt_token_count: int = 0, candidates_token_count: int = 0, response_text: str = "fake response"):
         self.usage_metadata = FakeMetadata()
         self.usage_metadata.prompt_token_count = prompt_token_count
         self.usage_metadata.candidates_token_count = candidates_token_count
--- main.py
+++ main.py
@@ -5,6 +5,27 @@ from google.genai import types
 import argparse
 from fakes import FakeMetadata, FakeResponse
 
+class ChatbotSettings():
+    def __init__(self):
+        def parse_inputs():
+            parser = argparse.ArgumentParser(description="Chatbot")
+            parser.add_argument("user_prompt", type=str, help="User prompt")
+            parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+            parser.add_argument("--debug", action="store_true", help="Skip the api call for debugging")
+            return parser.parse_args()
+        args = parse_inputs()
+        
+        self.verbose = args.verbose
+        self.debug_mode = args.debug
+        self.user_prompt = args.user_prompt
+
+        if self.verbose: 
+            print("\n" + "-"*50)
+            print(f"verbose: {self.verbose}")
+            print(f"debug_mode: {self.debug_mode}")
+            print(f"user_prompt: {self.user_prompt}")
+            print("-"*50 + "\n")
+
 def get_api_key():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -12,17 +33,11 @@ def get_api_key():
         raise RuntimeError("api key not found")
     return api_key
 
-def parse_inputs():
-    parser = argparse.ArgumentParser(description="Chatbot")
-    parser.add_argument("user_prompt", type=str, help="User prompt")
-    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
-    parser.add_argument("--debug", action="store_true", help="Skip the api call for debugging")
-    return parser.parse_args()
-
 def gemini_client(debug_mode: bool = False):
     model = "gemini-2.5-flash"
+    key = get_api_key()
     if not debug_mode:
-        client = genai.Client(api_key = get_api_key())
+        client = genai.Client(api_key = key)
     
     def gemini_response(messages):
         if not debug_mode:
@@ -33,11 +48,12 @@ def gemini_client(debug_mode: bool = False):
     return gemini_response
 
 def main():
-    args = parse_inputs()
-    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    response = gemini_client(args.debug)(messages=messages)
+    settings = ChatbotSettings()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=settings.user_prompt)])]
+    response = gemini_client(settings.debug_mode)(messages=messages)
     
-    if (response.usage_metadata is not None) and args.verbose:
+    if (response.usage_metadata is not None) and settings.verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     else:
