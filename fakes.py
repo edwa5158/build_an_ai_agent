dd3334b26332a74d0737002926c9a0fc27230cc7
edwa5158 <jasonedwards2011@gmail.com>
2025-12-23 13:36:40 -0600

    added fakes for the gemini response to allow testing without hitting usage limits

---
+++ fakes.py
@@ -0,0 +1,12 @@
+
+class FakeMetadata():
+    prompt_token_count: int = 0
+    candidates_token_count: int = 0
+
+class FakeResponse():
+    def __init__(self, prompt_token_count: int = 0, candidates_token_count: int = 0, response_text: str = ""):
+        self.usage_metadata = FakeMetadata()
+        self.usage_metadata.prompt_token_count = prompt_token_count
+        self.usage_metadata.candidates_token_count = candidates_token_count
+        self.text = response_text
+
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import argparse
+from fakes import FakeMetadata, FakeResponse
 
 def get_api_key():
     load_dotenv()
@@ -13,16 +15,29 @@ def get_api_key():
 def parse_inputs():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    parser.add_argument("--debug", action="store_true", help="Skip the api call for debugging")
     return parser.parse_args()
+
+def gemini_client(debug_mode: bool = False):
+    model = "gemini-2.5-flash"
+    if not debug_mode:
+        client = genai.Client(api_key = get_api_key())
+    
+    def gemini_response(messages):
+        if not debug_mode:
+            response = client.models.generate_content(model = model , contents = messages)
+        else:
+            response = FakeResponse(prompt_token_count=69, candidates_token_count=96)
+        return response
+    return gemini_response
 
 def main():
     args = parse_inputs()
-    client = genai.Client(api_key = get_api_key())
-    model = "gemini-2.5-flash"
-    contents = args.user_prompt
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    response = gemini_client(args.debug)(messages=messages)
     
-    response = client.models.generate_content(model = model , contents = contents)
-    if response.usage_metadata is not None:
+    if (response.usage_metadata is not None) and args.verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     else:
