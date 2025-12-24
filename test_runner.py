543c1d29f86c0040b8e220347dd0ae2d13425e0e
edwa5158 <jasonedwards2011@gmail.com>
2025-12-24 06:51:30 -0600

    rewrote the test runner as a list comprehension to address concerns about mutating the result var

--- test_runner.py
+++ test_runner.py
@@ -10,10 +10,7 @@ def run_all_tests():
         test_get_file_content.main,
         test_write_file.main,
     ]
-    result: str = ""
-    for func in test_modules:
-        result += "\n\n" + func()
-    
+    result: str = "\n\n".join([func() for func in test_modules])
     print(result)
 
 if __name__ == "__main__":
